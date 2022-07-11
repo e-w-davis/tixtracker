526b9ce303929c6c07fbf9dedf2396c464246ff3
Eric W. Davis <e.w.davis15@gmail.com>
2022-07-11 12:48:14 -0400

    WIP: app initialized and signup working

---
+++ main_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+# from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Tix(models.Model):
+    event_name = models.CharField(max_length=100)
+    venue = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
