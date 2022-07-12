e0c42d1c64f353e759a3e547c0b623ac5b288a6c
Eric W. Davis <e.w.davis15@gmail.com>
2022-07-12 14:15:29 -0400

    Sign out should work

--- tix_tracker_correct/settings.py
+++ tix_tracker_correct/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/tix/'
 
-LOG_OUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
